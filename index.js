a0355137ab13e970dd49d3c8e60405bab6a3346a
jasminro <77788006+jasminro@users.noreply.github.com>
2021-12-03 11:00:03 +0200

    New index

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ var cors = require("cors");
 app.use(cors());
 const port = process.env.PORT || 8080;
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+//const db = [{ name: "tiina" }, { name: "jack" }];
 
+
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+/*
 app.get("/names", (req, res) => {
   res.send(db);
 });
+*/
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
